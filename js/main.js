d4c9c1ec6396df6f9f380922d910f600a37853df
EslamElsebaey <eslamelsebaey.11@gmail.com>
2022-04-18 16:45:23 +0200

    first creation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function myfun() {
+  document.getElementById("myheader").style.backgroundColor = "#005CB4";
+  document.querySelector(".detailsimg img").src = "images/pepsi001.png";
+}
+function myfun2() {
+  document.getElementById("myheader").style.backgroundColor = "#C9002B";
+  document.querySelector(".detailsimg img").src = "images/pepsi002.png";
+}
+function myfun3() {
+  document.getElementById("myheader").style.backgroundColor = "black";
+  document.querySelector(".detailsimg img").src = "images/pepsi003.png";
+}
